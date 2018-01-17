6d74ff04b28f20aa3234c2b89ea9f1a474045a69
Eric <Eric.Ahn@gsacapital.com>
2018-01-17 10:56:20 +0000

    Copies 2.0 addon files to 2.1 addon directory

---
+++ addon21/css.py
@@ -0,0 +1,78 @@
+# syntax.py
+
+import sys
+
+# import the main window object (mw) from aqt
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo
+# import all of the Qt GUI library
+from aqt.qt import *
+
+
+def format(color, style=''):
+    """Return a QTextCharFormat with the given attributes.
+    """
+    _color = QColor()
+    _color.setNamedColor(color)
+
+    _format = QTextCharFormat()
+    _format.setForeground(_color)
+    if 'bold' in style:
+        _format.setFontWeight(QFont.Bold)
+    if 'italic' in style:
+        _format.setFontItalic(True)
+
+    return _format
+
+
+# Syntax styles that can be shared by all languages
+STYLES = {
+    'brace': format('#BB7977'),
+    'selector': format('#800000', 'bold'),
+    'property': format('#BB7977', 'bold'),
+    'equals': format('gray'),
+    'value': format('#074726')
+}
+
+
+class CssHighlighter (QSyntaxHighlighter):
+    """Syntax highlighter for the Python language.
+    """
+
+    def __init__(self, document):
+        QSyntaxHighlighter.__init__(self, document)
+
+        rules = []
+
+        rules += [(brace, 0, STYLES['brace'])
+                  for brace in ['\{', '\}']]
+
+        rules += [(r'((?:(?:(?:[\w\d-]+)?[#\.])?[\w\d-]+[\s,]+)+)\{', 1, STYLES['selector'])]
+        rules += [(r'\b([\w-]+)\s*:\s*([^;]*);', 1, STYLES['property'])]
+        rules += [(r'\b([\w-]+)\s*:\s*([^;]*);', 2, STYLES['value'])]
+
+        # Build a QRegExp for each pattern
+        self.rules = [(QRegExp(pat), index, fmt)
+                      for (pat, index, fmt) in rules]
+
+    def highlightBlock(self, text):
+        """Apply syntax highlighting to the given block of text.
+        """
+        # Do other syntax formatting
+        for expression, nth, format in self.rules:
+            index = expression.indexIn(text, 0)
+
+            while index >= 0:
+                # We actually want the index of the nth match
+                index = expression.pos(nth)
+                length = len(expression.cap(nth))
+                if expression == self.rules[-3][0]:
+                    print(text[index:index+30])
+                    print(length)
+                    print(expression.capturedTexts())
+                    print('\n')
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
+
+        self.setCurrentBlockState(0)
---
+++ addon21/html.py
@@ -0,0 +1,86 @@
+# syntax.py
+
+import sys
+
+# import the main window object (mw) from aqt
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo
+# import all of the Qt GUI library
+from aqt.qt import *
+
+
+def format(color, style=''):
+    """Return a QTextCharFormat with the given attributes.
+    """
+    _color = QColor()
+    _color.setNamedColor(color)
+
+    _format = QTextCharFormat()
+    _format.setForeground(_color)
+    if 'bold' in style:
+        _format.setFontWeight(QFont.Bold)
+    if 'italic' in style:
+        _format.setFontItalic(True)
+
+    return _format
+
+
+# Syntax styles that can be shared by all languages
+STYLES = {
+    'tag': format('#008000'),
+    'element': format('#008000', 'bold'),
+    'attribute': format('#7D9029'),
+    'equals': format('#7D9029'),
+    'value': format('#BA2121'),
+    'moustache': format('#BC7A00', 'bold')
+}
+
+
+class HtmlHighlighter (QSyntaxHighlighter):
+    """Syntax highlighter for the Python language.
+    """
+
+    def __init__(self, document):
+        QSyntaxHighlighter.__init__(self, document)
+
+        rules = []
+
+        rules += [(tag, 0, STYLES['tag'])
+                  for tag in ['<', '</', '>', '/>']]
+
+        rules += [(r'<\/?([A-Za-z0-9_]+)>?', 1, STYLES['element'])]
+        rules += [(r'\b([A-Za-z0-9_]+)(=)("[^"]+")', 1, STYLES['attribute'])]
+        rules += [(r'\b([A-Za-z0-9_]+)(=)("[^"]+")', 2, STYLES['equals'])]
+        rules += [(r'\b([A-Za-z0-9_]+)(=)("[^"]+")', 3, STYLES['value'])]
+
+        rules += [(r'\{\{[#^\/]?(\w+:)?\w+\}\}', 0, STYLES['moustache'])]
+
+        # Build a QRegExp for each pattern
+        self.rules = [(QRegExp(pat), index, fmt)
+                      for (pat, index, fmt) in rules]
+
+    def highlightBlock(self, text):
+        """Apply syntax highlighting to the given block of text.
+        """
+        # Do other syntax formatting
+        for expression, nth, format in self.rules:
+            index = expression.indexIn(text, 0)
+
+            while index >= 0:
+                # We actually want the index of the nth match
+                index = expression.pos(nth)
+                length = len(expression.cap(nth))
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
+
+        self.setCurrentBlockState(0)
+
+    def match_multiline(self, text, delimiter, in_state, style):
+        """Do highlighting of multi-line strings. ``delimiter`` should be a
+        ``QRegExp`` for triple-single-quotes or triple-double-quotes, and
+        ``in_state`` should be a unique integer to represent the corresponding
+        state changes when inside those strings. Returns True if we're still
+        inside a multi-line string when this function is finished.
+        """
+        pass
